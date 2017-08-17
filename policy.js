3f25797315666aa7fa4c7b4a78843253b2808da2
nntruong122 <nguyen.truong122@gmail.com>
2017-08-17 10:59:23 +0700

    Update policy.js

--- policy.js
+++ policy.js
@@ -1,1 +1,11 @@
 
+/**
+ * http://usejsdoc.org/
+ */
+var PolicyInfoModule = new Object();
+
+PolicyInfoModule.sayhello  = function (){
+	console.log("PolicyModule","Hello Policy");
+};
+
+module.exports = ConversationModule;
