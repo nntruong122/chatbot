c8356aff6d616f62f15a82f4bd71c0d3cb69fcb6
nntruong122 <nguyen.truong122@gmail.com>
2017-08-22 13:32:47 +0700

    commit v6

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
-
+var tag_option = 0
+var tag_op_option=0
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -40,7 +41,7 @@ app.get('/webhook/', function (req, res) {
 /*v2*/
 // All callbacks for Messenger will be POST-ed here
 app.post("/webhook", function (req, res) {
-
+	console.log(JSON.stringify(req.body));
   // Make sure this is a page subscription
   if (req.body.object == "page") {
     // Iterate over each entry
@@ -52,19 +53,20 @@ app.post("/webhook", function (req, res) {
           processPostback(event);
         }
        else if (event.message) {
-         console.log(JSON.stringify(event.message));
-         if( event.message.quick_reply){
-           var quick_reply = event.message.quick_reply;
-           var senderId  = event.sender.id;
-           var payload = quick_reply.payload;
-           console.log(payload);
-           if (payload){
-             processPayload(senderId, payload);
-           }
-         }else {
-        
-            processMessage(event);
-         }
+    	   console.log(JSON.stringify(event.message));
+    	   if( event.message.quick_reply){
+    		   var quick_reply = event.message.quick_reply;
+    		   var senderId  = event.sender.id;
+    		   var payload = quick_reply.payload;
+    		   console.log(payload);
+    		   if (payload){
+    			   processPayload(senderId, payload);
+    		   }
+    	   }else {
+    	  
+    		   
+    		   	processMessage(event);
+    	   }
         }
         
       });
@@ -83,6 +85,10 @@ app.post("/webhook", function (req, res) {
 
 //sends message to User on FBMessenger
 function sendMessageToFBMessenger(recipientId, message) {
+	
+ // Save curent step of SenderID	
+ console.log('recipientId ', recipientId)
+ //
   request({
     url: "https://graph.facebook.com/v2.6/me/messages",
     qs: {access_token: process.env.FB_ACCESS_TOKEN},
@@ -90,8 +96,10 @@ function sendMessageToFBMessenger(recipientId, message) {
     json: {
       recipient: {id: recipientId},
       message: message,
+      
     }
   },  function(error, response, body) {
+	  
       if (error) {
           console.log('Error sending messages: ', error)
       } else if (response.body.error) {
@@ -119,10 +127,10 @@ function getFullNameCUS(senderId){
                 var bodyObj = JSON.parse(body);
                 var first_name = bodyObj.first_name;
                 var lastname = bodyObj.last_name;
-                fullname =   first_name; 
+                fullname =   first_name + " "+ lastname; 
 
               }});
-        console.log ("jsbody", fullname);
+        console.log ("jsbody full name", fullname);
     } catch( err){
         console.log("error call getFullNameCUS ",err );
     }
@@ -190,11 +198,11 @@ function processMessage(event) {
    
        switch (message.text) {
           case 'generic':
-            sendGenericMessage(senderId);
+          
             break;
 
           default:
-             sendMessageToFBMessenger(senderId, {text: message.text});
+          sendMessageToFBMessenger(senderId, {text: message.text});
         }
       
     } else if (message.attachments) {
@@ -202,72 +210,6 @@ function processMessage(event) {
     }
   }
 }
-function sendGenericMessage(sender) {
-    messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "Ai Chat Bot Communities",
-                    "subtitle": "Communities to Follow",
-                    "image_url": "http://1u88jj3r4db2x4txp44yqfj1.wpengine.netdna-cdn.com/...",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.facebook.com/groups/aic...",
-                        "title": "FB Chatbot Group"
-                    }, {
-                        "type": "web_url",
-                        "url": "https://www.reddit.com/r/Chat_Bots/",
-                        "title": "Chatbots on Reddit"
-                    },{
-                        "type": "web_url",
-                        "url": "https://twitter.com/aichatbots",
-                        "title": "Chatbots on Twitter"
-                    }],
-                }, {
-                    "title": "Chatbots FAQ",
-                    "subtitle": "Aking the Deep Questions",
-                    "image_url": "https://tctechcrunch2011.files.wordpress.com/...",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "What's the benefit?",
-                        "payload": "Chatbots make content interactive instead of static",
-                    },{
-                        "type": "postback",
-                        "title": "What can Chatbots do",
-                        "payload": "One day Chatbots will control the Internet of Things! You will be able to control your homes temperature with a text",
-                    }, {
-                        "type": "postback",
-                        "title": "The Future",
-                        "payload": "Chatbots are fun! One day your BFF might be a Chatbot",
-                    }],
-                },  {
-                    "title": "Learning More",
-                    "subtitle": "Aking the Deep Questions",
-                    "image_url": "http://www.brandknewmag.com/wp-cont...",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "AIML",
-                        "payload": "Checkout Artificial Intelligence Mark Up Language. Its easier than you think!",
-                    },{
-                        "type": "postback",
-                        "title": "Machine Learning",
-                        "payload": "Use python to teach your maching in 16D space in 15min",
-                    }, {
-                        "type": "postback",
-                        "title": "Communities",
-                        "payload": "Online communities & Meetups are the best way to stay ahead of the curve!",
-                    }],
-                }]  
-            } 
-        }
-    }
-  
-    
-    sendMessageToFBMessenger(sender, messageData);
-}
-
 // process wellcome 
 function sendPostBackWellcome (senderId, body){
     
@@ -294,38 +236,38 @@ function sendPostBackWellcome (senderId, body){
     // send image hello 
     
   request({
-      url: "https://graph.facebook.com/v2.6/me/messages",
-      qs: {access_token: process.env.FB_ACCESS_TOKEN},
-      method: "POST",
-      json: {
-        recipient: {id: senderId},
-        message: messageData,
-      }
-    },  function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', response.body.error)
-        } else {
-            //send question support
-            // send quick set button
-            var quicksetButton  = {
-                  "text":messageHello,
-                  "quick_replies":[
-                    {
-                      "content_type":"text",
-                      "title":"Có",
-                      "payload":"yes_support_welcome"
-                    },
-                    {
-                      "content_type":"text",
-                      "title":"Không",
-                      "payload":"no_support_welcome"
-                    }
-                  ]
-                }
-            sendMessageToFBMessenger(senderId, quicksetButton);
-            //end 
-        }
-    });
+	    url: "https://graph.facebook.com/v2.6/me/messages",
+	    qs: {access_token: process.env.FB_ACCESS_TOKEN},
+	    method: "POST",
+	    json: {
+	      recipient: {id: senderId},
+	      message: messageData,
+	    }
+	  },  function(error, response, body) {
+	      if (error) {
+	          console.log('Error sending messages: ', response.body.error)
+	      } else {
+	    	  	//send question support
+	    	    // send quick set button
+	    	    var quicksetButton  = {
+	    	    	    "text":messageHello,
+	    	    	    "quick_replies":[
+	    	    	      {
+	    	    	        "content_type":"text",
+	    	    	        "title":"Có",
+	    	    	        "payload":"yes_support_welcome"
+	    	    	      },
+	    	    	      {
+	    	    	        "content_type":"text",
+	    	    	        "title":"Không",
+	    	    	        "payload":"no_support_welcome"
+	    	    	      }
+	    	    	    ]
+	    	    	  }
+	    	    sendMessageToFBMessenger(senderId, quicksetButton);
+	    	    //end 
+	      }
+	  });
   } catch (err)  {
       console.log('error sendPostBackWellcome: ',err);
   }
@@ -336,52 +278,34 @@ function sendPostBackWellcome (senderId, body){
 function processPayload (senderId,payload){
     try 
     {
+    	tag_option = 0;
+    	console.log("tag_op_option: ", tag_op_option );
+    	
         switch (payload) {
         case "yes_support_welcome":
 
-          //policyInfo.sayhello();
+        	//policyInfo.sayhello();
             //show log
             console.log("start processPayload action:", "yes_support_welcome" );
             var messageText = "SupportBot có thể giúp bạn các vấn đề nào sao đây?"
-            var messageData= {
-                "attachment":{
-                    "type":"template",
-                    "payload":{
-                        "template_type":"button",
-                        "text":messageText,
-                        "buttons":[
-                                {
-                                    "type":"postback",
-                                    "title":"Hợp đồng và phí",
-                                    "payload":"op_policy"
-                                },
-                                {
-                                    "type":"postback",
-                                    "title":"Mua thêm hợp đồng",
-                                    "payload":"op_buy"
-                                }
-                        ]
-                    }
-                }   
-            };
-            
+          
             
             // send quick set button
-          var quicksetButton  = {
-                "text":messageText,
-                "quick_replies":[
-                  {
-                    "content_type":"text",
-                    "title":"Hợp đồng và phí",
-                    "payload":"op_policy"
-                  },
-                  {
-                    "content_type":"text",
-                    "title":"Mua thêm hợp đồng",
-                    "payload":"op_buy"
-                  }
-                ]
-              };
+    	    var quicksetButton  = {
+    	    	    "text":messageText,
+    	    	    "quick_replies":[
+    	    	      {
+    	    	        "content_type":"text",
+    	    	        "title":"Hợp đồng và phí",
+    	    	        "payload":"op_policy"
+    	    	      },
+    	    	      {
+    	    	        "content_type":"text",
+    	    	        "title":"Mua thêm hợp đồng",
+    	    	        "payload":"op_buy"
+    	    	      }
+    	    	    ]
+    	    	  };
             
             sendMessageToFBMessenger(senderId, quicksetButton);
             break;
@@ -392,6 +316,35 @@ function processPayload (senderId,payload){
             
             
             break;
+            
+        case "op_policy":
+        	// Get full name 
+        	tag_op_option = 1;
+        	tag_option = tag_op_option;
+        	console.log("Start op_policy");
+        	var fullname ="";
+        	  request({
+        	      url: "https://graph.facebook.com/v2.6/" + senderId,
+        	      qs: {
+        	        access_token: token,
+        	        fields: "first_name,last_name"
+        	      },
+        	      method: "GET"
+        	    }, function(error, response, body) {
+        	      var greeting = "";
+        	      if (error) {
+        	        console.log("Error getting user's name: " +  error);
+        	      } else {
+        	        var bodyObj = JSON.parse(body);
+        	        fullname = bodyObj.first_name  +" "+ bodyObj.last_name;
+        	        var messageText =  fullname + " ơi, bạn cho SupportBot xin số hợp đồng hoặc CMND của chủ hợp đồng bảo hiểm nhé (chỉ nhập số thôi nhé bạn). :)";
+        	        sendMessageToFBMessenger(senderId, {text: messageText});
+        	      }
+        	    });
+        	
+        	
+        	
+        	break;
         default:
             // To do
             sendMessageToFBMessenger(senderId, {text: "Sorry, I don't understand your request."});
